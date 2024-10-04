cd7b4a6fab0effeebbc9efb6dee3e5eee4531e10
Alexey Pupyshev <117092537+Nervus-Rerum@users.noreply.github.com>
2024-10-04 16:23:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# 1st program
+print((9 ** 0.5)*5)
+
+# 2hd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 < 2 * (2 + 2))
+
+# 4th program
+a = '123.456'
+a = float(a) # Перевод в тип данных float
+print(type(a)) # Проверка типа данных
+a = a * 10
+print(a)
+a = int(a) # Перевод в тип данных int
+print(a)
+a = a % 10 # Остаточное деление на 10
+print(a)
